246f2ac17976f9565a3dce3ce93cc7259eeed8da
Dmitrii Chizhov <dimachig@gmail.com>
2022-01-08 14:14:35 +0300

    feat: reverse-int solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+
 module.exports = function reverse (n) {
-  
+    let digits = n.toString().split('');
+    console.log(digits);
+    if(digits[0] === '-') {
+        let minus = digits.slice(1);
+        console.log(minus);
+        minus = minus.reverse().join('');
+        minus = Number(minus);
+        return minus;
+    }
+    let num = digits.reverse().join('');
+    num = Number(num);
+    return num;
 }
+
