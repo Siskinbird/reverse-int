acdda2cf82e3c999ec79b160533a5c7a7ff598d9
Dmitrii Chizhov <59995752+Siskinbird@users.noreply.github.com>
2022-01-09 15:16:30 +0300

    fix; Delete console.log

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-
 module.exports = function reverse (n) {
     let digits = n.toString().split('');
-    console.log(digits);
     if(digits[0] === '-') {
         let minus = digits.slice(1);
-        console.log(minus);
         minus = minus.reverse().join('');
         minus = Number(minus);
         return minus;
